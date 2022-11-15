2a3950a3b6e2a5cf94ba72bbc79b38e7d21c4ec2
Antonio Coppe <antonio.coppe@gmail.com>
2022-11-15 12:33:19 +0100

    First Commit

---
+++ Entities/Employee.cs
@@ -0,0 +1,9 @@
+namespace WireMockDemo.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? Department { get; set; }
+    }
+}
